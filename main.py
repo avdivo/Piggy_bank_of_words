54cb2817d55dd4f5b4c4fe65ce5d02bd7a22a282
Alexey <avdivo@mail.ru>
2021-08-26 01:56:09 +0300

    Работает, пока без перевода

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
+import nltk
+from collections import Counter
+from translate import Translator
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+url = 'https://flask-user.readthedocs.io/en/latest/'
+url = 'https://lengish.com/texts/text-1.html'
 
+response = requests.get(url).content
+soup = BeautifulSoup(response, "html.parser")
+words = nltk.tokenize.word_tokenize(soup.get_text())
+lemm = nltk.stem.WordNetLemmatizer()
+words = Counter([lemm.lemmatize(word.lower()) for word in words if word.isalpha() and len(word) > 2])
+print(words)
+print(len(words))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+translator = Translator(from_lang="english",to_lang="russian")
+cast = sorted([(word, cou) for word, cou in words.items()], key=lambda item: item[1], reverse=True)
+print(*cast, sep='\n')
